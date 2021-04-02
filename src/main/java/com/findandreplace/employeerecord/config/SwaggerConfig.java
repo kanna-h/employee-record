6b7112b4c0ba581859547342b0cc0ad458ddffae
KANNA <harikrishna010203@gmail.com>
2021-04-02 09:13:47 -0400

    HATEOAS-Swagger: LinkDiscoverer exception resolved, updating the Swagger to 3.X

--- src/main/java/com/findandreplace/employeerecord/config/SwaggerConfig.java
+++ src/main/java/com/findandreplace/employeerecord/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.findandreplace.employeerecord.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
@@ -15,19 +16,24 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    //LinkDiscovery exception resolved after updating the swagger dependency.
+    //De-activating the bean definition.
+    /*
+    @Primary
     @Bean
     public LinkDiscoverers discoverers() {
         List<LinkDiscoverer> plugins = new ArrayList<>();
         plugins.add(new CollectionJsonLinkDiscoverer());
         return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
     }
-
+*/
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -46,6 +52,7 @@ public class SwaggerConfig {
                 "Terms of service url",
                 new Contact("Mr. Lazy Programmer", "Application url", "lazy@programmer.com"),
                 "API or Application License",
-                "License Url");
+                "License Url",
+                Collections.emptyList());
     }
 }
