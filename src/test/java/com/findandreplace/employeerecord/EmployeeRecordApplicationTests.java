835ab3f37e25483683cccf5dfe2f6c49514e11fd
KANNA <harikrishna010203@gmail.com>
2021-04-03 04:05:41 -0400

    Introduced: Maven Build Profiles - skip test

--- src/test/java/com/findandreplace/employeerecord/EmployeeRecordApplicationTests.java
+++ src/test/java/com/findandreplace/employeerecord/EmployeeRecordApplicationTests.java
@@ -3,7 +3,7 @@ package com.findandreplace.employeerecord;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EmployeeRecordApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EmployeeRecordApplicationTests {
 
 	@Test
